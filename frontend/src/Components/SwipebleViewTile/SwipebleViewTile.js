15c1fafc86f172f42ce6407bc71ad487fa3545b9
Denis G <guly.denis2014@yandex.ru>
2022-12-31 01:51:34 +0300

    Aded my group and schedule for rooms and teachers

---
+++ frontend/src/Components/SwipebleViewTile/SwipebleViewTile.js
@@ -0,0 +1,244 @@
+import { useEffect, useRef, useState } from "react";
+
+import "../../App.css";
+
+export default function SwipebleViewTile({ isRoom, isTeachers, subjects }) {
+    const tileRef = useRef(null);
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+//     let childs = null;
+//     let rect = null;
+//     let timer = null;
+// //     const [isScrolled, setIsScrolled] = useState(false);
+// const isScrolled = useRef(false)
+
+//     const scrollHandler2 = () => {
+      
+//         const scrollLeft = tileRef.current.scrollLeft;
+//         console.log(scrollLeft)
+//         //if (childs?.length > 1) {
+//         let pos = scrollLeft / rect.width;
+//         console.log(scrollLeft)
+//         setCurrentSlide(pos);
+//         tileRef.current.scrollLeft = scrollLeft;
+//         //}
+//     };
+
+//     const scrollHandler = () => {
+//         if (timer !== null) {
+//             clearTimeout(timer);
+//         }
+//         timer = setTimeout(function () {
+//             // do something
+//             // setIsScrolled(true)
+//             isScrolled.current = true
+//         }, 550);
+//     };
+
+//     useEffect(() => {
+//       childs = tileRef.current.children;
+//         rect = tileRef.current.getBoundingClientRect();
+//       const scrollLeft = tileRef.current.scrollLeft;
+//         //if (childs?.length > 1) {
+//         let pos = tileRef.current.scrollLeft / tileRef.current.getBoundingClientRect().width;
+//         setCurrentSlide(pos)
+//         console.log(scrollLeft)
+//         setIsScrolled(false)
+//     }, [isScrolled])
+
+//     useEffect(() => {
+//         childs = tileRef.current.children;
+//         rect = tileRef.current.getBoundingClientRect();
+//         //   document.addEventListener("touchstart", handleTouchStart, false);
+//         //   document.addEventListener("touchmove", handleTouchMove, false);
+//         //   document.addEventListener(
+//         //       "scroll",
+//         //       function (e) {
+//         //             if (isContainerScrolling)
+//         //                   e.preventDefault();
+//         //           console.log("document scrolling");
+//         //       },
+//         //       false
+//         //   );
+//     });
+
+    return (
+        <>
+            <div
+                className={
+                    "subject-tile-container" +
+                    (subjects?.length > 1 ? " scrollable" : "")
+                }
+                key={
+                    new Date() +
+                    (subjects &&
+                        subjects[0]["номерЗанятия"] + subjects[0]["начало"])
+                }
+            //     ref={tileRef}
+                // onScroll={scrollHandler}
+            //     onScroll={scrollHandler}
+            >
+                {subjects?.map((subject) => (
+                    <SubjectTile isTeachers={isTeachers} isRoom={isRoom} subject={subject} />
+                ))}
+            </div>
+            <DotIndicator currentSlide={currentSlide} subjects={subjects} />
+        </>
+    );
+}
+
+function DotIndicator({ currentSlide, subjects }) {
+//     useEffect(() => {
+//         console.log(Math.round(currentSlide, 1));
+//     }, []);
+    return (
+        <div className="dot-indicator">
+            {subjects?.length > 1 &&
+                subjects.map((subject, index) => (
+                    <div
+                        className={
+                            "" +
+                            (Math.round(currentSlide, 1) === index
+                                ? "current"
+                                : "")
+                        }
+                    />
+                ))}
+        </div>
+    );
+}
+
+function SubjectTile({ isRoom, isTeachers, subject }) {
+    const types = [
+        { name: "Военная кафедра", color: "#fff" },
+        { name: "лаб", color: "#E4E9FF" },
+        { name: "лек", color: "#DBFFE7" },
+        { name: "пр.", color: "#FFE2E7" },
+        { name: "фв", color: "#FFF8E1" },
+        { name: "экз", color: "#AADAFF" },
+        { name: "зач", color: "#F2BAA6" },
+        { name: "зчО", color: "#F2BAA6" },
+    ];
+
+    const getStyle = (name) => {
+        for (let i = 0; i < types.length; i++)
+            if (name.split(" ")[0].includes(types[i].name))
+                return types[i].color;
+        return "";
+    };
+
+    return (
+        <div className="subject-tile" key={subject["код"]}>
+            <div
+                className="subject-tile-left"
+                style={{
+                    background: getStyle(subject["дисциплина"]),
+                }}
+            >
+                <h1>{subject["номерЗанятия"]}</h1>
+                <h4 className="time-h">
+                    с <span className="time-span">{subject["начало"]}</span>
+                </h4>
+                <h4 className="time-h">
+                    до <span className="time-span">{subject["конец"]}</span>
+                </h4>
+            </div>
+
+            <div className="subject-tile-right">
+                <h3>{subject["дисциплина"]}</h3>
+                {isRoom || isTeachers && <p>Группа {subject["группа"]}</p>}
+                <p>Аудитория {subject["аудитория"]}</p>
+                <p>{subject["преподаватель"]}</p>
+            </div>
+        </div>
+    );
+}
+
+// let timer = null;
+//     let isContainerScrolling = false;
+
+//     let xDown = null;
+//     let yDown = null;
+
+//     const getTouches = (evt) => {
+//         return (
+//             evt.touches || // browser API
+//             evt.originalEvent.touches
+//         ); // jQuery
+//     };
+
+//     const handleTouchStart = (evt) => {
+//         const firstTouch = getTouches(evt)[0];
+//         xDown = firstTouch.clientX;
+//         yDown = firstTouch.clientY;
+
+//         const rect = tileRef.current.getBoundingClientRect();
+//         if (
+//             !(
+//                 xDown >= rect.left &&
+//                 xDown <= rect.right &&
+//                 yDown <= rect.bottom &&
+//                 yDown >= rect.top
+//             )
+//         ) {
+//             /* reset values */
+//             xDown = null;
+//             yDown = null;
+//         }
+//     };
+
+//     const handleTouchMove = (evt) => {
+//         if (!xDown || !yDown) {
+//             return;
+//         }
+
+//         let xUp = evt.touches[0].clientX;
+//         let yUp = evt.touches[0].clientY;
+
+//         let xDiff = xDown - xUp;
+//         let yDiff = yDown - yUp;
+
+//         const rect = tileRef.current.getBoundingClientRect();
+
+//         console.log(xDiff, yDiff);
+
+//         if (Math.abs(xDiff) > Math.abs(yDiff)) {
+//             /*most significant*/
+//             if (xDiff > 0) {
+//                 /* right swipe */
+//                 console.log("RIGHT");
+//                 //tileRef.current.style.transform = 'translateX(' + rect.width +'px)'
+//                 // tileRef.current.scrollLeft = rect.width
+//             } else {
+//                 /* left swipe */
+//             }
+//         } else {
+//             if (yDiff > 0) {
+//                 /* down swipe */
+//             } else {
+//                 /* up swipe */
+//             }
+//         }
+//         /* reset values */
+//         xDown = null;
+//         yDown = null;
+//     };
+
+//     const scrollHandler = () => {
+//         //   const rect = tileRef.current.getBoundingClientRect();
+//         //   console.log(tileRef.current.scrollLeft);
+//         //   if (tileRef.current.scrollLeft > rect.width / 2) {
+//         //       tileRef.current.style.left = rect.width;
+//         //   }
+//         console.log("tile scrolling");
+//         isContainerScrolling = true;
+//         if (timer !== null) {
+//             clearTimeout(timer);
+//         }
+//         timer = setTimeout(function () {
+//             // do something
+//             console.log("tile stop scrolling");
+//             isContainerScrolling = false;
+//         }, 150);
+//     };
--- frontend/src/Components/Views/ViewRoom.jsx
+++ frontend/src/Components/Views/ViewRoom.jsx
@@ -1,10 +1,10 @@
-import CalendarComponent from "./CalendarComponent";
+import CalendarComponent from "../CalendarComponent";
 //import {Typography} from "antd";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment/moment";
-import SwipebleViewTile from "./SwipebleViewTile/SwipebleViewTile";
+import SwipebleViewTile from "../SwipebleViewTile/SwipebleViewTile";
 
 const month = [
     "Января",
@@ -21,13 +21,14 @@ const month = [
     "Декабря",
 ];
 
-const dayOfWeek = [];
+const requests = {
+    group: "https://edu.donstu.ru/api/Rasp?idGroup=",
+    room: "https://edu.donstu.ru/api/Rasp?idAudLine=",
+    teachers: "https://edu.donstu.ru/api/Rasp?idTeacher=",
+};
 
-export default function View() {
-    const navigate = useNavigate();
-    const { groupId } = useParams();
-    //const id = 44464
-
+export default function ViewRoom({ isTeachers, isRoom }) {
+    const { roomId } = useParams();
     const [rasp, setRasp] = useState([]);
     const [info, setInfo] = useState({});
     const [groupedRasp, setGroupedRasp] = useState({});
@@ -35,43 +36,10 @@ export default function View() {
 
     const [date, setDate] = useState(new Date());
     const todayDate = date.getDate();
-    const [inFavorites, setInFavorites] = useState(false);
 
     const normalize = (value) => {
         if (value < 10) return "0" + value;
         else return value;
-    };
-
-    const scheduleProccessing1 = (res) => {
-        let obj = {};
-        let rasp1 = res.data.data.rasp;
-        for (let i = 0; i < rasp1.length; i++) {
-            if (obj[rasp1[i]["дата"].split("T")[0]]?.length > 0) {
-                if (
-                    rasp1[i]["номерЗанятия"] === rasp1[i - 1]["номерЗанятия"] &&
-                    rasp1[i - 1]["дата"].split("T")[0] ===
-                        rasp1[i]["дата"].split("T")[0]
-                ) {
-                    obj[rasp1[i]["дата"].split("T")[0]].push({
-                        ...rasp1[i],
-                        isPodgr: true,
-                    });
-                } else {
-                    obj[rasp1[i]["дата"].split("T")[0]].push({
-                        ...rasp1[i],
-                        isPodgr: false,
-                    });
-                }
-            } else {
-                obj[rasp1[i]["дата"].split("T")[0]] = [];
-                obj[rasp1[i]["дата"].split("T")[0]].push({
-                    ...rasp1[i],
-                    isPodgr: false,
-                });
-            }
-        }
-
-        return obj;
     };
 
     const scheduleProccessing = (res) => {
@@ -110,13 +78,22 @@ export default function View() {
         return obj;
     };
 
+    const getRequestUrl = () => {
+        let url = "";
+
+        if (isTeachers) url = requests.teachers;
+        if (isRoom) url = requests.room;
+
+        return url;
+    };
+
     const updateSchedule = (currentDate) => {
         setIsLoaded(false);
 
         axios
             .get(
-                "https://edu.donstu.ru/api/Rasp?idGroup=" +
-                    groupId +
+                getRequestUrl() +
+                    roomId +
                     `&sdate=${currentDate.getFullYear()}-${normalize(
                         currentDate.getMonth() + 1
                     )}-${normalize(currentDate.getDate())}`
@@ -132,37 +109,14 @@ export default function View() {
             });
     };
 
-    const checkFavorites = () => {
-        setInFavorites(false);
-        const favoritesGroups = JSON.parse(localStorage.getItem("favorites"));
-        // console.log(info);
-        if (favoritesGroups) {
-            favoritesGroups.forEach((favorites) => {
-                if (favorites.id === info?.group?.groupID) {
-                    setInFavorites(true);
-                    return;
-                }
-            });
-        }
-    };
-
     useEffect(() => {
-        axios
-            .get("https://edu.donstu.ru/api/GetRaspDates?idGroup=" + groupId)
-            .then((res) => {
-                const data = res.data.data.dates;
-            });
-
         updateSchedule(new Date());
     }, []);
-
-    let array = [];
 
     useEffect(() => {
         const doc = document.getElementById(todayDate);
 
         document.title = info.group?.name + " - Расписание MySecrets";
-        checkFavorites();
 
         if (doc) doc.scrollIntoView({ behavior: "smooth", block: "start" });
     }, [isLoaded === true]);
@@ -172,73 +126,22 @@ export default function View() {
             <div className="tiles-container">
                 <header>
                     <div style={{ padding: "0 1.5em" }}></div>
-                    <h2 className="title-h2">
-                        Группа {isLoaded && info.group.name}
-                    </h2>
-
-                    {isLoaded && (
-                        <div
-                            className={
-                                "icon-btn" + (inFavorites ? " favorite" : "")
-                            }
-                            onClick={() => {
-                                let favoritesGroups = JSON.parse(
-                                    localStorage.getItem("favorites")
-                                );
-
-                                if (inFavorites) {
-                                    if (favoritesGroups.length > 0) {
-                                        let myArray = favoritesGroups.filter(
-                                            function (obj) {
-                                                return (
-                                                    obj.id !==
-                                                    info.group.groupID
-                                                );
-                                            }
-                                        );
-                                        console.log(myArray);
-                                        localStorage.setItem(
-                                            "favorites",
-                                            JSON.stringify(myArray)
-                                        );
-                                    }
-                                } else {
-                                    if (favoritesGroups === null)
-                                        favoritesGroups = [];
-                                    favoritesGroups.push({
-                                        name: info.group.name,
-                                        id: info.group.groupID,
-                                        facul: "",
-                                    });
-                                    console.log(favoritesGroups);
-                                    localStorage.removeItem("favorites");
-                                    localStorage.setItem(
-                                        "favorites",
-                                        JSON.stringify(favoritesGroups)
-                                    );
-                                }
-
-                                checkFavorites();
-                            }}
-                        >
-                            <svg
-                                width="800px"
-                                height="800px"
-                                viewBox="-5.5 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="m0 2.089v21.912l6.546-6.26 6.545 6.26v-21.912c-.012-1.156-.952-2.089-2.109-2.089-.026 0-.051 0-.077.001h.004-8.726c-.022-.001-.047-.001-.073-.001-1.158 0-2.098.933-2.109 2.088v.001z" />
-                            </svg>
-                        </div>
+                    {isTeachers && (
+                        <h2 className="title-h2">
+                            Преподаватель {isLoaded && info.prepod.name}
+                        </h2>
+                    )}
+                    {isRoom && (
+                        <h2 className="title-h2">
+                            Аудитория {isLoaded && info.aud.name}
+                        </h2>
                     )}
                 </header>
                 <CalendarComponent updateSchedule={updateSchedule} />
-                
                 {isLoaded ? (
                     <>
                         {Object.keys(groupedRasp).length > 0 ? (
                             <>
-                            
                                 {Object.keys(groupedRasp).map((gr) => (
                                     <div
                                         id={Number(gr.split("-")[2]).toString()}
@@ -267,6 +170,8 @@ export default function View() {
                                             // ava
                                             // </div>
                                             <SwipebleViewTile
+                                                isRoom={isRoom}
+                                                isTeachers={isTeachers}
                                                 subjects={subjects}
                                             />
                                         ))}
